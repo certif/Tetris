3b004fb8bb64c035c5f3921d24e99324a4c9de3e
Oleksandr Tkachov <certifis@gmail.com>
2019-11-17 01:35:18 +0200

    feat: add rotation of game elements

--- src/game.js
+++ src/game.js
@@ -27,7 +27,7 @@ export default class Game {
   activePiece = {
     x: 0,
     y: 0,
-    blocks: [
+    blocks: [  
       [0,1,0],
       [1,1,1],
       [0,0,0]
@@ -56,6 +56,39 @@ export default class Game {
     if (this.hasCollision()) {
         this.activePiece.y -= 1;
         this.lockPiece();
+    }
+  }
+
+  rotatePiece() {
+    this.rotateBlocks();
+
+    if (this.hasCollision()) {
+      this.rotateBlocks(false);
+    }
+  }
+
+  rotateBlocks(clockwise = true) {
+    const blocks = this.activePiece.blocks;
+    const length = blocks.length;
+    const x = Math.floor(length /2);
+    const y = length -1;
+
+    for (let i = 0; i < x; i++) {
+      for (let j = i; j < y - i; j++) {
+        const temp = blocks[i][j];
+
+        if (clockwise) {
+          blocks[i][j] = blocks[y - j][i];
+          blocks[y - j][i] = blocks[y - i][y - i];
+          blocks[y - i][y - j] = blocks[j][y - i];
+          blocks[j][y - i] = temp;
+        } else {
+          blocks[i][j] = blocks[j][y - i];
+          blocks[j][y - i] = blocks[y - i][y - j];
+          blocks[y - i][y - j] = blocks[y - j][i];
+          blocks[y - j][i] = temp;
+        }
+      }
     }
   }
 
