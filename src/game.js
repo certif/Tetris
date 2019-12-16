8489acc9b0a2e58efe862bc816ea66a3f067ce0c
Oleksandr Tkachov <certifis@gmail.com>
2019-12-16 22:17:16 +0200

    feat: implement delete lines and score

--- src/game.js
+++ src/game.js
@@ -1,10 +1,20 @@
 export default class Game {
+  static points = {
+    '1': 40,
+    '2': 100,
+    '3': 300,
+    '4': 1200
+  };
+
   score = 0;
   lines = 0;
-  level = 0;
   playfield = this.createPlayfield();
   activePiece = this.createPiece();
   nextPiece = this.createPiece();
+
+  get level() {
+    return Math.floor(this.lines * 0.1);
+  }
 
   getState() {
     const playfield = this.createPlayfield();
@@ -114,7 +124,7 @@ export default class Game {
     this.activePiece.x -= 1;
 
     if (this.hasCollision()) {
-        his.activePiece.x += 1;
+        this.activePiece.x += 1;
     }
   }
 
@@ -132,6 +142,8 @@ export default class Game {
     if (this.hasCollision()) {
         this.activePiece.y -= 1;
         this.lockPiece();
+        const clearedLines = this.clearLines();
+        this.updateScore(clearedLines);
         this.updatePieces();
     }
   }
@@ -197,8 +209,44 @@ export default class Game {
       }
     } 
   }
+
+  clearLines() {
+    const rows = 20;
+    const columns = 10;
+    let lines = [];
+
+    for (let y = rows - 1; y >= 0; y--) {
+      let numberOfBlocks = 0;
+
+      for (let x = 0; x < columns; x++) {
+        if (this.playfield[y][x]) {
+          numberOfBlocks +=1;
+        }
+      }
+      if (numberOfBlocks === 0) {
+        break;
+      } else if (numberOfBlocks < columns) {
+        continue;
+      } else if (numberOfBlocks === columns) {
+        lines.unshift(y);
+      }
+    }
+    for (let index of lines) {
+      this.playfield.splice(index, 1);
+      this.playfield.unshift(new Array(columns).fill(0));
+    }
+    return lines.length;
+  }
+
+  updateScore(clearedLines) {
+    if (clearedLines > 0) {
+      this.score += Game.points[clearedLines] * (this.level + 1);
+      this.lines += clearedLines;
+    }
+  }
+
   updatePieces() {
     this.activePiece = this.nextPiece;
-    this.nextPiece = this.createPiece;
+    this.nextPiece = this.createPiece();
   }
 }
